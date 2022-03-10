892ecdbf5cc94bd0c7be1ba40ceb46e7b18a5a6e
ibenchi <horizen27@gmail.com>
2022-03-10 21:32:19 +0000

    Button Click

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,9 @@ class Browser {
     await page.type("#text-input-what", "React Developer")
     await page.waitForSelector("#text-input-where")
     await page.type("#text-input-where", "Canada")
+    // const button = await page.(document.querySelector("button.yosegi-InlineWhatWhere-primaryButton")! as HTMLButtonElement)
+    await page.waitForSelector(".yosegi-InlineWhatWhere-primaryButton")
+    await page.click(".yosegi-InlineWhatWhere-primaryButton")
   }
 }
 //text-input-what
